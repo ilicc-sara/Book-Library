a10856aa35555b28d1b3d6279d76dad1af5235e9
Sara <sarailic431@gmail.com>
2025-01-24 19:36:05 +0100

    html and css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const newBookBtn = document.querySelector(".new-btn");
+const form = document.querySelector(".form");
+const overlay = document.querySelector(".overlay");
+
+newBookBtn.addEventListener("click", function () {
+  form.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
